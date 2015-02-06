2ee466ada54b19bfcab9e7ffb90c1d37e874a39d
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-02-06 15:40:46 +0900

    Check that digits were properly created.

---
+++ check.d/900GPSMuon5GeVThruHits.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+INPUT=elecsim-200GPSMuon5GeVThruHits.root
+
+# Check that the pmt digit container was created
+if ! (dump-event.exe -O dump ${INPUT} | grep 'TDigitContainer.*pmt'); then
+    echo FAILED Missing pmt digit container in ${INPUT}
+    exit 1
+fi
+
+# Check the number of PMTs created
+if [ $(dump-event.exe -O dump ${INPUT} | grep 'MC-P-' | wc -l) -lt 5 ]; then
+    echo FAILED Missing pmt hits in ${INPUT}
+    exit 1
+fi
+
+# Check that the drift digit container was created
+if ! (dump-event.exe -O dump ${INPUT} | grep 'TDigitContainer.*drift'); then
+    echo FAILED Missing drift digit container in ${INPUT}
+    exit 1
+fi
+
+# Check the number of X wires created
+if [ $(dump-event.exe -O dump ${INPUT} | grep 'MC-W-X-' | wc -l) -lt 10 ]; then
+    echo FAILED Missing X wire hits in ${INPUT}
+    exit 1
+fi
+
+# Check the number of U wires created
+if [ $(dump-event.exe -O dump ${INPUT} | grep 'MC-W-U-' | wc -l) -lt 10 ]; then
+    echo FAILED Missing U wire hits in ${INPUT}
+    exit 1
+fi
+
+# Check the number of V wires created
+if [ $(dump-event.exe -O dump ${INPUT} | grep 'MC-W-V-' | wc -l) -lt 10 ]; then
+    echo FAILED Missing V wire hits in ${INPUT}
+    exit 1
+fi
+
+echo SUCCESS
